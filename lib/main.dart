d6b45d284e14c01d464f663aaadfd405235c57f1
vincent.min <vincent.min@b12-consulting.com>
2024-06-15 20:24:36 +0000

    working prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+// import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,79 +16,31 @@ class _MyAppState extends State<MyApp> {
   Color selectedColor = Colors.black;
   List<List<Offset>> paths = [];
 
-  void _setColor(Color color) {
-    setState(() {
-      selectedColor = color;
-    });
-  }
-
-  void _clearPaths() {
-    setState(() {
-      paths.clear();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Directionality(
         textDirection: TextDirection.ltr,
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Drawing App'),
-            actions: [
-              // IconButton(
-              //   icon: const Icon(Icons.color_lens),
-              //   onPressed: () => _showColorPicker(context),
-              // ),
-              IconButton(
-                icon: const Icon(Icons.clear),
-                onPressed: _clearPaths,
-              ),
-            ],
-          ),
           body: GestureDetector(
-            onPanUpdate: (details) => setState(() {
-              if (paths.isNotEmpty) {
-                paths.last.add(details.globalPosition);
-              } else {
-                paths.add([details.globalPosition]); // Start a new path if there are none
-              }
+            onPanStart: (details) => setState(() {
+              paths.add(<Offset>[details.localPosition]);
             }),
+            onPanUpdate: (details) => setState(() {
+              paths.last.add(details.localPosition);
+            }),
+            // onPanEnd: (details) => setState(() {
+            //   paths.add(<Offset>[]);
+            // }),
             child: CustomPaint(
               painter: DrawingPainter(paths, selectedColor),
+              child: Container(),
             ),
           ),
         )
       )
     );
   }
-
-  
-  // void _showColorPicker(BuildContext context) {
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         title: const Text('Pick a color'),
-  //         content: SingleChildScrollView(
-  //           child: ColorPicker(
-  //             pickerColor: selectedColor,
-  //             onColorChanged: _setColor,
-  //           ),
-  //         ),
-  //         actions: <Widget>[
-  //           ElevatedButton(
-  //             child: const Text('Got it'),
-  //             onPressed: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-  // }
 
 }
 
@@ -100,9 +52,20 @@ class DrawingPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = color
+      ..strokeWidth = 25.0
+      ..style = PaintingStyle.stroke; // Use stroke style for drawing lines
+
     for (var path in paths) {
-      Paint paint = Paint()..color = color..strokeWidth = 25;
-      canvas.drawPath(Path()..addPolygon(path, true), paint);
+      Path drawingPath = Path();
+      if (path.isNotEmpty) {
+        drawingPath.moveTo(path.first.dx, path.first.dy);
+        for (var point in path.skip(1)) {
+          drawingPath.lineTo(point.dx, point.dy);
+        }
+      }
+      canvas.drawPath(drawingPath, paint);
     }
   }
 
