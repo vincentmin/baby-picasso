003447ee79879421780972b6ceb12570b8051ca7
VincentMin <vincentmin17@gmail.com>
2024-06-16 20:06:11 +0200

    remove unused color attribute

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Color color = Colors.black;
   List<PathColorPair> paths = [];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Scaffold(
-          body: GestureDetector(
-            onPanStart: (details) => setState(() {
-              paths.add(PathColorPair([details.localPosition]));
-            }),
-            onPanUpdate: (details) => setState(() {
-              paths.last.points.add(details.localPosition);
-            }),
-            child: CustomPaint(
-              painter: DrawingPainter(paths),
-              child: Container(),
-            ),
-          ),
-        )
-      )
-    );
+        home: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Scaffold(
+              body: GestureDetector(
+                onPanStart: (details) => setState(() {
+                  paths.add(PathColorPair([details.localPosition]));
+                }),
+                onPanUpdate: (details) => setState(() {
+                  paths.last.points.add(details.localPosition);
+                }),
+                child: CustomPaint(
+                  painter: DrawingPainter(paths),
+                  child: Container(),
+                ),
+              ),
+            )));
   }
-
 }
