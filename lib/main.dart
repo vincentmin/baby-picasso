3b4f2a7b2a624d5cb1b6bd1bc0d2c114e4459578
vincentmin <vincentmin17@gmail.com>
2024-06-17 23:31:24 +0200

    first attempt

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,48 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   List<PathColorPair> paths = [];
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool _isAnimating = false;
+  double _radius = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1000.0).animate(_controller)
+      ..addListener(() {
+        setState(() {
+          _radius = _animation.value;
+        });
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            _isAnimating = false;
+            paths.clear(); // Clear paths when animation completes
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    setState(() {
+      _isAnimating = true;
+    });
+    _controller.forward(from: 0.0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +63,56 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         body: GestureDetector(
-          onPanStart: (details) => setState(() {
-            paths.add(PathColorPair([details.localPosition]));
-          }),
-          onPanUpdate: (details) => setState(() {
-            paths.last.points.add(details.localPosition);
-          }),
-          child: CustomPaint(
-            painter: DrawingPainter(paths),
-            child: Container(),
-          ),
-        ),
+            onPanStart: (details) {
+              if (!_isAnimating) {
+                setState(() {
+                  paths.add(PathColorPair([details.localPosition]));
+                });
+              }
+            },
+            onPanUpdate: (details) {
+              if (!_isAnimating) {
+                setState(() {
+                  paths.last.points.add(details.localPosition);
+                });
+              }
+            },
+            child: ClipPath(
+              clipper: _isAnimating ? CircleRevealClipper(_radius) : null,
+              child: CustomPaint(
+                painter: DrawingPainter(paths),
+                child: Container(),
+              ),
+            )),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() {
-            paths.clear();
-          }),
+          onPressed: _startAnimation,
           child: const Icon(Icons.refresh),
         ),
       ),
     );
+  }
+}
+
+class CircleRevealClipper extends CustomClipper<Path> {
+  final double radius;
+  CircleRevealClipper(this.radius);
+
+  @override
+  Path getClip(Size size) {
+    Path outerPath = Path()
+      ..addOval(Rect.fromLTWH(0, 0, size.width / 2, size.height / 2));
+
+    Path innerPath = Path()
+      ..addOval(Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: radius,
+      ));
+
+    return Path.combine(PathOperation.xor, outerPath, innerPath);
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
